9d97156188268b3286c70178c8b853e64cb64545
Sbeier27 <s.beier27@ncf.edu>
2025-12-12 04:17:08 -0500

    Added in art files and changed name of Landing page to Index so github pages functions

---
+++ Shop/ShopJS.js
@@ -0,0 +1,48 @@
+/*on off for the Heat and bleach poster*/
+function on1(){
+    document.getElementById("overlay1").style.display="block";
+}
+function off1(){
+    document.getElementById("overlay1").style.display="block";
+}
+
+/*on off vaporbeach poster*/
+function on2(){
+    document.getElementById("overlay2").style.display="block";
+}
+function off2(){
+    document.getElementById("overlay2").style.display="block";
+}
+
+/* On and off for cherry limeade poster*/
+function on3(){
+    document.getElementById("overlay3").style.display="block";
+}
+function off3(){
+    document.getElementById("overlay3").style.display="block";
+
+}
+
+/* On and off for sour bitch poster*/
+function on4(){
+    document.getElementById("overlay4").style.display="block";
+}
+function off4(){
+    document.getElementById("overlay4").style.display="block";
+}
+
+/*on off feelings poster*/
+function on5(){
+    document.getElementById("overlay5").style.display="block";
+}
+function off5(){
+    document.getElementById("overlay5").style.display="block";
+}
+
+/*on off mountains poster*/
+function on6(){
+    document.getElementById("overlay6").style.display="block";
+}
+function off6(){
+    document.getElementById("overlay6").style.display="block";
+}
